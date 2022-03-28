ffdbbd11ff520c992f132321d28c97362a485fb2
DerrickMac <macarand@oregonstate.edu>
2022-03-28 15:10:41 -0700

    update 1 of 1.

---
+++ animal.py
@@ -0,0 +1,8 @@
+# Author: Derrick Macaranas
+# GitHub username: DerrickMac
+# Date: 3/28/2022
+# Description: Asks the user for their favorite animal and then
+#              prints out that that is their favorite animal
+
+fave_animal = input("Please enter your favorite animal: ")
+print("Your favorite animal is the " + fave_animal + ".")
